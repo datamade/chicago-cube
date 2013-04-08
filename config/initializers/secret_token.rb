52e440276d00711f9753144f2a4eb1d5ca4e7986
Derek Eder <derek.eder@gmail.com>
2013-04-08 11:12:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChicagoCube::Application.config.secret_token = 'de4c9dd47d9afc691a1d246b238b530b368dddbfa48bf95c2713d80d0713c7dc84d280a044d3354f5e33903bfcd9a1296d9d179360c2314a4e9049447e2521a9'
