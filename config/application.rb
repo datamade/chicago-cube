04b40201ef665d858e2c1c197c8c608e48fbad08
Nick Doiron <ndoiron@ndoiron-VirtualBox.(none)>
2013-04-09 11:25:13 -0500

    patch heroku config

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module ChicagoCube
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku fix for asset pipeline
+    config.assets.initialize_on_precompile = false
   end
 end
